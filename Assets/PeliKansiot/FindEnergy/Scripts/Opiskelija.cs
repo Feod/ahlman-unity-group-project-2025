c0bdc7c8d5755e78a6617067fb90f4c5f11f4935
Feod <miikkaharjuntausta@gmail.com>
2025-02-20 14:51:49 +0200

    Energiajuoma Tweenaukset

--- Assets/PeliKansiot/FindEnergy/Scripts/Opiskelija.cs
+++ Assets/PeliKansiot/FindEnergy/Scripts/Opiskelija.cs
@@ -32,16 +32,28 @@ public class Opiskelija : MonoBehaviour
     public void MuutaOpiskelijanSprite(bool iloinen)
     {
 
+        //A
+        LeanTween.scale(opiskelijaRenderer.gameObject, opiskelijaRenderer.transform.localScale * 1.05f, 0.4f).setEase(LeanTweenType.punch);
+
         if(iloinen == true)
         {
+            //B
+
             opiskelijaRenderer.sprite = spriteIloinenOpiskelija;
         }
         else
         {
+            //C
+            LeanTween.scale(Haavejuomarenderer.gameObject, Haavejuomarenderer.transform.localScale * 1.5f, 0.6f).setEase(LeanTweenType.punch);
+            LeanTween.rotateZ(Haavejuomarenderer.gameObject, Random.Range(-90f,90),1f).setEase(LeanTweenType.punch);
+
             opiskelijaRenderer.sprite = spriteSurullinenOpiskelija;
         }
-
+        //D
     }
+
+    
+
 
 
     
